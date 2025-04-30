e30ea25fc8a4227abcbcac168ce712e34db7acbd
DMSlobodianiuk <63192578+DMSlobodianiuk@users.noreply.github.com>
2025-04-30 18:02:50 +0300

    Update CopyTask.py

--- CopyTask.py
+++ CopyTask.py
@@ -2,9 +2,7 @@ import shutil
 import os
 
 def main():
-    # os.system('scp "D:\LFJ\TEST AUTOMATION INTERNSHIP\Tasks\Test1.txt" dms@192.168.229.130:/home/dms/TestsFolder/') # копиіюємо лише 1 файл
-    # os.system('scp -r "D:\LFJ\TEST AUTOMATION INTERNSHIP\Tasks" dms@192.168.229.130:/home/dms/TestsFolder/') # копіюємо папку
-    print("Enter file(s) or directory which you want to copy into VM")
+    print("Enter file(s) or directory that you want to copy into VM")
     command = 'scp -r '
     source = input("objects to move: ")
     command += source
@@ -22,13 +20,3 @@ if __name__ == "__main__":
 
 
 
-# destination = os.path.join(destination, os.path.basename(source)) # копіює одразу папку
-
-# dest = shutil.copytree(source,destination,dirs_exist_ok=True) #копіює файл
-
-# scp "D:\LFJ\TEST AUTOMATION INTERNSHIP\Tasks\Test1.txt" dms@192.168.229.130:/home/dms/
-
-
-#"D:\LFJ\TEST AUTOMATION INTERNSHIP\Tasks\Test1.txt"
-#D:\LFJ\TEST AUTOMATION INTERNSHIP\Test_folder\Test1.txt
-#dms@192.168.229.130:/home/dms/TestsFolder/
